558f44306da0891831dba87c75b59bbc12057e33
mickyKay92 <36381900+mickyKay92@users.noreply.github.com>
2018-02-12 02:09:08 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    
+/*var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myPic');
+var modalImg = document.getElementById("pic1");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}*/
+
+  $(".nav-item a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 192
+        
+      }, 1000, function() {
+      });
+    }
+  });
+});
